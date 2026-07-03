879601a3255f315754dae0ec2dc5a9cf4ab89639
javadocq <han18264@naver.com>
2026-07-03 20:10:03 +0900

    feat: rawMaterial Entity

---
+++ Models/MasterData/RawMaterial.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mes_server.Models.MasterData
+{
+    public class RawMaterial
+    {
+        [Key]
+        [MaxLength(20)]
+        public string MaterialID { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(50)] 
+        public string MaterialName { get; set;} = string.Empty;
+
+        [Required]
+        public int StockQty { get; set; }
+    }
+}
