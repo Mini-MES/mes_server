8e1fadddf4af41e2f5621e59655ba36fa9b36b3d
javadocq <han18264@naver.com>
2026-07-03 20:05:49 +0900

    feat: processMaster Entity

---
+++ Models/MasterData/ProcessMaster.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mes_server.Models.MasterData
+{
+    public class ProcessMaster
+    {
+        [Key]
+        public int ProcessID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string ProcessName { get; set; } = string.Empty;
+
+        [Required]
+        public int SequenceOrder { get; set; }
+    }
+}
