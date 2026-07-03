897f78255802db7139e4ad7d2556de1beaa6472f
javadocq <han18264@naver.com>
2026-07-03 20:21:38 +0900

    feat: bom Entity

---
+++ Models/MasterData/BOM.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mes_server.Models.MasterData
+{
+    public class BOM
+    {
+        [Required]
+        [MaxLength(50)]
+        public string ProductID { get; set; } = string.Empty;
+
+        [ForeignKey("ProductID")]
+        public ProductMaster? Product { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string MaterialID { get; set; } = string.Empty;
+
+        [ForeignKey("MaterialID")]
+
+        public RawMaterial? Material { get; set; }
+        [Required]
+        public int RequiredQty { get; set; }
+
+    }
+}
