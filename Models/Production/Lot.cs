8cb66b10bbc11a6573afa9e9124ef8d17af2ca3b
javadocq <han18264@naver.com>
2026-07-03 20:40:32 +0900

    feat: lot Entity

---
+++ Models/Production/Lot.cs
@@ -0,0 +1,28 @@
+﻿using mes_server.Models.MasterData;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mes_server.Models.Production
+{
+    public class Lot
+    {
+        [Key]
+        public string LotID { get; set; } = string.Empty;
+
+        [Required]
+        public int OrderID { get; set; }
+
+        [ForeignKey("OrderID")]
+        public WorkOrder? WorkOrder { get; set; }
+
+        [Required]
+        public int CurrentProcessID { get; set; }
+
+        [ForeignKey("CurrentProcessID")]
+        public ProcessMaster? CurrentProcess { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Status { get; set; } = "Running";
+    }
+}
