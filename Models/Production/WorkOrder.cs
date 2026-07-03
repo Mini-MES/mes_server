d8cfc135522c2dd3e112df249f3331c2013f3a75
javadocq <han18264@naver.com>
2026-07-03 20:30:10 +0900

    feat: workOrder Entity

---
+++ Models/Production/WorkOrder.cs
@@ -0,0 +1,31 @@
+﻿using mes_server.Models.MasterData;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mes_server.Models.Production
+{
+    public class WorkOrder
+    {
+        [Key]
+        public int OrderId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string ProductID { get; set; } = string.Empty;
+
+        [ForeignKey("ProductID")]
+        public ProductMaster? Product { get; set; }
+
+        [Required]
+        public int TargetQty { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime DueDate { get; set; }
+    }
+}
