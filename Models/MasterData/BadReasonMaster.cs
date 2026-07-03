ec41808ca6431fc9b934e6e879f79d4845c1c2e6
javadocq <han18264@naver.com>
2026-07-03 20:15:32 +0900

    feat: badReasonMaster ENtity

---
+++ Models/MasterData/BadReasonMaster.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mes_server.Models.MasterData
+{
+    public class BadReasonMaster
+    {
+        [Key]
+        [MaxLength(20)]
+        public string ReasonCode { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string ReasonDescription { get; set; } = string.Empty;
+    }
+}
