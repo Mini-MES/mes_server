065edbd6967c9f3f1fcf8eca5e8804e37d03201b
javadocq <han18264@naver.com>
2026-07-03 20:03:28 +0900

    feat: user entity

---
+++ Models/MasterData/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mes_server.Models.MasterData
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required]
+        public string UserRole { get; set; } = "Operator";
+
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
