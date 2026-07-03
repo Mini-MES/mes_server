c0204003519e3bfcb0187aad452784fa5c0a5c51
javadocq <han18264@naver.com>
2026-07-03 20:46:13 +0900

    feat: tool Entity

---
+++ Models/Production/Tool.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mes_server.Models.Production
+{
+    public class Tool
+    {
+        [Key]
+        [Required]
+        public string ToolID { get; set; } = null!;
+
+        [Required]
+        public string ToolName { get; set; } = null!;
+
+        [Required]
+        public int MaxUsageCount { get; set; }
+
+        [Required]
+        public int CurrentUsageCount { get; set; }
+    }
+}
