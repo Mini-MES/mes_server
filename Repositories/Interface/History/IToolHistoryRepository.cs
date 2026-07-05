3aa9f552d6665518fadff134dd91431782436568
javadocq <han18264@naver.com>
2026-07-05 17:23:16 +0900

    feat: toolHistoryRepository

--- Repositories/Interface/History/IToolHistoryRepository.cs
+++ Repositories/Interface/History/IToolHistoryRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace mes_server.Repositories.Interface.History
+﻿using mes_server.Models.History;
+using mes_server.Repositories.Interface.Generic;
+
+namespace mes_server.Repositories.Interface.History
 {
-    public interface IToolHistoryRepository
+    public interface IToolHistoryRepository : IGenericRepository<ToolHistory>
     {
+        Task<IEnumerable<ToolHistory>> GetHistoryByToolIdAsync(string toolId);
     }
 }
