efee574f6444f4f0dd86de896ab211f5a1703481
javadocq <han18264@naver.com>
2026-07-05 17:17:04 +0900

    feat: bomRepository

---
+++ Repositories/Interface/MasterData/IBOMRepository.cs
@@ -0,0 +1,10 @@
+﻿using mes_server.Models.MasterData;
+using mes_server.Repositories.Interface.Generic;
+
+namespace mes_server.Repositories.Interface.MasterData
+{
+    public interface IBOMRepository : IGenericRepository<BOM>
+    {
+        Task<IEnumerable<BOM>> GetBOMByProductIdAsync(string productId);
+    }
+}
