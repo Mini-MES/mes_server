1a328bad482ad57f7f8981da8a14166c05e2a034
javadocq <han18264@naver.com>
2026-07-06 22:45:59 +0900

    feat: userService

---
+++ Repositories/MasterData/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using mes_server.Data;
+using mes_server.Models.MasterData;
+using mes_server.Repositories.Generic;
+using mes_server.Repositories.Interface.MasterData;
+using Microsoft.EntityFrameworkCore;
+
+namespace mes_server.Repositories.MasterData
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        public UserRepository(MESDbContext context) : base(context)
+        {
+        }
+
+        public async Task<User?> GetByUserNameAsync(string userName)
+        {
+            return await Context.Users.FirstOrDefaultAsync(x => x.UserName == userName);
+        }
+    }
+}
---
+++ Services/Interface/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using mes_server.Models.MasterData;
+
+namespace mes_server.Services.Interface
+{
+    public interface IUserService
+    {
+        Task RegisterUserAsync(User user, string password);
+        Task<bool> AuthenticateAsync(string userName, string password);
+        Task UpdateUserRoleAsync(string userId, string newRole);
+    }
+}
