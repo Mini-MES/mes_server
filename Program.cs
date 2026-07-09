2259c14fd7732f580b144398911986218c75a554
javadocq <han18264@naver.com>
2026-07-09 18:00:24 +0900

    feat: program setting

--- Program.cs
+++ Program.cs
@@ -9,7 +9,10 @@ using mes_server.Repositories.MasterData;
 using mes_server.Repositories.Production;
 using mes_server.Services;
 using mes_server.Services.Interface;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace mes_server
 {
@@ -18,6 +21,7 @@ namespace mes_server
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
             var connectionString = builder.Configuration.GetConnectionString("MESDbConnection")
                  ?? throw new InvalidOperationException("Connection string 'MESDbConnection' was not found.");
             builder.Services.AddDbContext<MESDbContext>(options => options.UseSqlServer(connectionString));
@@ -45,9 +49,41 @@ namespace mes_server
             builder.Services.AddControllers();
             builder.Services.AddSwaggerGen();
 
+
+            // 4. 인증 관련
+            var JwtSettings = builder.Configuration.GetSection("Jwt");
+            var key = Encoding.ASCII.GetBytes(JwtSettings["Key"]);
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true, // 만료 시간 체크
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = JwtSettings["Issuer"],
+                    ValidAudience = JwtSettings["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ClockSkew = TimeSpan.Zero // 만료 시간 오차 없이 즉시 만료되도록 설정
+                };
+            });
+
+            builder.Services.AddAuthorization();
+            builder.Services.AddControllers();
+
             var app = builder.Build();
 
-            if(app.Environment.IsDevelopment())
+            // 미들웨어 등록 (순서 중요)
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
