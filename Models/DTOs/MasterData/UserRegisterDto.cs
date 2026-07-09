8dd7e2a1f4abb601e2c8c45c92d3c17060f2bc58
javadocq <han18264@naver.com>
2026-07-09 17:51:50 +0900

    feat: user dto

---
+++ Models/DTOs/MasterData/UserRegisterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace mes_server.Models.DTOs.MasterData
+{
+    public class UserRegisterDto
+    {
+        public string UserID { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+        public string Password { get; set; } = null!
+    }
+}
