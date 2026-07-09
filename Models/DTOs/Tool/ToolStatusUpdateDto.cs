4622c2109709e939da10bcf83338655281135e39
javadocq <han18264@naver.com>
2026-07-09 10:59:04 +0900

    fix: tool dto

---
+++ Models/DTOs/Tool/ToolStatusUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿using mes_server.Models.Enum;
+
+namespace mes_server.Models.DTOs.Tool
+{
+    public class ToolStatusUpdateDto
+    {
+        public ToolStatus Status { get; set; }
+        public ReasonCode Reason { get; set; }
+    }
+}
