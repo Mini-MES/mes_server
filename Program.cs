c51b0b12bec8c26b7fec6c9e8f6c4622a15caa71
javadocq <han18264@naver.com>
2026-07-16 13:11:12 +0900

    feat: cookie extract jwt

--- Program.cs
+++ Program.cs
@@ -91,6 +91,17 @@ namespace mes_server
                     IssuerSigningKey = new SymmetricSecurityKey(key),
                     ClockSkew = TimeSpan.Zero // 만료 시간 오차 없이 즉시 만료되도록 설정
                 };
+                options.Events= new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        if (context.Request.Cookies.ContainsKey("token"))
+                        {
+                            context.Token = context.Request.Cookies["token"];
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
             });
             builder.Services.AddSwaggerGen(c =>
             {
