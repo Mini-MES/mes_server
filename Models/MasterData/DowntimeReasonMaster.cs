9a64fc31030032fd1c1494ffa5fdc5744e8524ef
javadocq <han18264@naver.com>
2026-08-05 00:41:54 +0900

    feat: downtimeReasonMaster class

---
+++ Models/MasterData/DowntimeReasonMaster.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mes_server.Models.MasterData
+{
+    public class DowntimeReasonMaster
+    {
+        [Key]
+        [MaxLength(50)]
+        public string ReasonCode { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string ReasonName { get; set; } = string.Empty;
+
+        [MaxLength(50)]
+        public string Category { get; set; } = "GENERAL";
+
+        public bool IsActive { get; set; } = true; // 비가동 사유 사용 여부
+    }
+}
