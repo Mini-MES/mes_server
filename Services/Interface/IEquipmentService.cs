7df6affe0487b0d23931ee78eef3d298ef401f8d
javadocq <han18264@naver.com>
2026-08-05 01:17:55 +0900

    feat: IEquipmentService

---
+++ Services/Interface/IEquipmentService.cs
@@ -0,0 +1,15 @@
+﻿using mes_server.Models.DTOs.MasterData;
+using mes_server.Models.MasterData;
+
+namespace mes_server.Services.Interface
+{
+    public interface IEquipmentService
+    {
+        Task<IEnumerable<EquipmentDto>> GetAllEquipmentAsync();
+        Task<EquipmentDto?> GetEquipmentByIdAsync(string equipmentId);
+        Task<bool> ChangeEquipmentStatusAsync(ChangeEquipmentStatusRequest request);
+        Task<bool> RegisterDowntimeReasonAsync(RegisterDowntimeReasonRequest request);
+        Task<IEnumerable<DowntimeReasonDto>> GetDowntimeReasonsAsync();
+        Task<IEnumerable<DowntimeLog>> GetDowntimeLogsByEquipmentAsync(string equipmentId);
+    }
+}
