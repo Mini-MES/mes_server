d8fdba879e4cc5aba3287d9a3d4725cd2e98b7fc
javadocq <han18264@naver.com>
2026-08-06 22:13:03 +0900

    feat: oee dto

---
+++ Models/DTOs/MasterData/OEEStatsDto.cs
@@ -0,0 +1,27 @@
+﻿namespace mes_server.Models.DTOs.MasterData
+{
+    public class EquipmentOeeDto
+    {
+        public string EuquipmentId { get; set; } = null!;
+        public string EquipmentName { get; set; } = null!;
+        public string status { get; set; } = null!;
+        public double TotalRunningMinutes { get; set; }
+        public double TotalDowntimeMinutes { get; set; }
+        public double AvailabilityRate { get; set; } // 가동률
+        public double PerformanceRate { get; set; } // 성능률
+        public double QualityRate { get; set; } // 품질률
+        public double OEE { get; set; } // OEE
+    }
+
+    public class OEESummaryDto
+    {
+        public double AverageAvailabilityRate { get; set; } 
+        public double AveragePerformanceRate { get; set; } // 평균 성능률
+        public double AverageQualityRate { get; set; } // 평균 품질률
+        public double AverageOEE { get; set; } // 평균 OEE
+
+        public int TotalEquipmentCount { get; set; } // 총 장비 수
+        public int RunningEquipmentCount { get; set; }
+        public int StoppedEquipmentCount { get; set; }
+    }
+}
