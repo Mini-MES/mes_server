99ded0ee45af8da96ca2651a964a50bcb6411ab6
javadocq <han18264@naver.com>
2026-08-14 00:09:54 +0900

    feat: opcUaSerivce interface

---
+++ Services/Interface/IOpcUaService.cs
@@ -0,0 +1,10 @@
+﻿namespace mes_server.Services.Interface
+{
+    public interface IOpcUaService
+    {
+        event Action<string, object, DateTime> DataChanged;
+        Task ConnectAndSubscribeAsync();
+        Task DisconnectAsync();
+        bool IsConnected { get; }
+    }
+}
